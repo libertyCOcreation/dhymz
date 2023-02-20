d14810cf95241d6e1d24e7acbf67a1304d29ab46
liberty-mori-toshiaki <mori@liberty-innovation.co.jp>
2023-02-20 13:31:44 +0900

    subまで作成し、大幅に修正しました

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
+// c o n t e n t s
 // content_wrapがviewport に入ったらfade in
 const container = document.querySelector(".container");
 
 window.addEventListener("scroll", () => {
-  const content_wrap = document.querySelector(".content_wrap");
   const scroll_value = window.scrollY;
   console.log("container_scrollTop", scroll_value);
+
+  const contents_wrap = document.querySelector(".contents_wrap");
+  const sub_contents_wrap = document.querySelector(".sub_contents_wrap");
 
   // scrollTop 0 -> visible_mode を外す
   if (scroll_value == 0) {
     console.log("clear!");
-    if (content_wrap.classList.contains("visible_mode")) {
-      content_wrap_visible();
+    if (contents_wrap.classList.contains("visible_mode_1")) {
+      content_wrap_visible_1();
+    }
+    if (sub_contents_wrap.classList.contains("visible_mode_2")) {
+      content_wrap_visible_2();
     }
   }
 
@@ -18,17 +24,33 @@ window.addEventListener("scroll", () => {
   if (scroll_value > 10) {
     console.log("fire!");
     // すでに付加済みの場合はスキップ
-    if (content_wrap.classList.contains("visible_mode")) {
+    if (contents_wrap.classList.contains("visible_mode_1")) {
     } else {
-      content_wrap_visible();
+      content_wrap_visible_1();
     }
   }
 });
 
-// contentsにある３つの画像にフェードインCSSを付加
-function content_wrap_visible() {
-  const content_wrap = document.querySelectorAll(".content_wrap");
-  for (let i = 0; content_wrap.length; i++) {
-    content_wrap[i].classList.toggle("visible_mode");
-  }
+// contentsにある３つの画像の表示/非表示
+function content_wrap_visible_1() {
+  const contents_wrap = document.querySelector(".contents_wrap");
+    contents_wrap.classList.toggle("visible_mode_1");
+}
+// sub_contentsの表示/非表示
+function content_wrap_visible_2() {
+  const sub_contents_wrap = document.querySelector(".sub_contents_wrap");
+  sub_contents_wrap.classList.toggle("visible_mode_2");
+}
+
+
+// s u b _ c o n t e n t s
+// 
+function open_subcontentent(open_no) {
+  const sub_contents_wrap = document.querySelector(".sub_contents_wrap");
+  sub_contents_wrap.classList.remove("visible_mode_2");
+  sub_contents_wrap.classList.add("visible_mode_2");
+  console.log(`choice no:${open_no}`);
+
+  // const sub_contents = document.querySelectorAll("sub_contents");
+  // sub_contents[open_no].classList.add("choice");
 }
